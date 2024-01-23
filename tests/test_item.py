5f837403b5c891fcdf4f824672cc84e5edc0192f
Сергей Арефьев <Simpson6515@yandex.ru>
2024-01-23 12:55:34 +0500

    added conftest.py file, added test_item.py file and code written in item.py file.

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,11 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import *
+
+
+def test_calculate_total_price(data):
+    assert Item.calculate_total_price(data) == 125000.00
+
+
+def test_apply_discount(data):
+    Item.apply_discount(data)
+    assert data.price == 12500.00
